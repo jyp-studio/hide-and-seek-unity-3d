6645116061aa4659f5d007cb8ec9ed85f759b23d
jyp-studio <jackie1129008@gmail.com>
2022-12-28 21:37:20 +0800

    first commit

---
+++ Assets/Scripts/GhostController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+
+public class GhostController : MonoBehaviour
+{
+    // moving
+    public Vector3 MovingDirection;
+    Rigidbody rb;
+    Animator animator;
+    [SerializeField] float movingSpeed = 0f;
+    [SerializeField] float JumpForce = 100f;
+    bool run = false;
+    public int SceneIndexDestination = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        run = false;
+
+        // move
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movingSpeed += 20f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            movingSpeed -= 20f;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            run = true;
+            transform.localPosition += movingSpeed * Time.deltaTime * Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            run = true;
+            transform.localPosition += movingSpeed * Time.deltaTime * Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            run = true;
+            transform.localPosition += movingSpeed * Time.deltaTime * Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            run = true;
+            transform.localPosition += movingSpeed * Time.deltaTime * Vector3.right;
+        }
+        animator.SetBool("Run", run);
+
+        // attack
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.SetTrigger("Shoot");
+        }
+    }
+
+    private void OnCollisionStay(UnityEngine.Collision collision)
+    {
+        if (collision.transform.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                // animator.SetTrigger("Jump");
+                rb.AddForce(JumpForce * Vector3.up);
+            }
+        }
+    }
+}
