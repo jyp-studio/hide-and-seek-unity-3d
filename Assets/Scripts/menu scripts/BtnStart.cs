35111b367b6cd35b919078b728e5c79856493c3d
jyp-studio <jackie1129008@gmail.com>
2022-12-29 22:01:06 +0800

    update menu buttons sound effects

--- Assets/Scripts/menu scripts/BtnStart.cs
+++ Assets/Scripts/menu scripts/BtnStart.cs
@@ -9,8 +9,19 @@ public class BtnStart : MonoBehaviour, IPointerClickHandler, IPointerEnterHandle
 {
     public TextMeshProUGUI btnStart;
     public int SceneIndexDestination = 1;
+    private AudioSource _audioSource;
+    public AudioClip selectingSound;
+    public AudioClip pushingSound;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
     public void OnPointerEnter(PointerEventData e)
     {
+        _audioSource.clip = selectingSound;
+        _audioSource.volume *= 3;
+        _audioSource.Play();
         btnStart.fontStyle = FontStyles.Underline | FontStyles.SmallCaps;
     }
     public void OnPointerExit(PointerEventData e)
@@ -20,6 +31,9 @@ public class BtnStart : MonoBehaviour, IPointerClickHandler, IPointerEnterHandle
 
     public void OnPointerClick(PointerEventData e)
     {
+        _audioSource.clip = pushingSound;
+        _audioSource.Play();
+
         // get current scene
         Scene scene = SceneManager.GetActiveScene();
 
